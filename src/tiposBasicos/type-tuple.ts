3a84a11d39a44f4c7582538ee9e18c56877694b7
walterjrr <jrwalter731@gmail.com>
2022-04-12 11:45:27 -0300

    type tuple

---
+++ src/tiposBasicos/type-tuple.ts
@@ -0,0 +1,10 @@
+const dadosCliente: readonly [number, string] = [1, 'Junior'];
+const dadosCliente1: [number, string, string?] = [1, 'Junior', 'Walter'];
+
+/*dadosCliente[0] = 100;
+dadosCliente[1] = '3';
+dadosCliente.pop();*/
+
+console.log(dadosCliente);
+
+dadosCliente1[1] = 'Walter';
