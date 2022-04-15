35860fe444d0a87af02749472ea40eb189e83c58
walterjrr <jrwalter731@gmail.com>
2022-04-15 16:17:57 -0300

    herança classe

--- src/classes&Poo/index.ts
+++ src/classes&Poo/index.ts
@@ -13,3 +13,27 @@ export class Person {
 }
 const person = new Person('Walter', 17);
 console.log(person.info());
+
+/**/
+
+export class UserAccount {
+  constructor(public name1: string, public age1: number) {}
+}
+
+class CharAccount extends UserAccount {
+  nickname1: string;
+  level1: number;
+
+  constructor(name1: string, age1: number, nick: string, level: number) {
+    super(name1, age1);
+
+    this.nickname1 = nick;
+    this.level1 = level;
+  }
+}
+
+const Walter = new UserAccount(`walter`, 17);
+console.log(Walter);
+
+const vitoria = new CharAccount('VITORIA', 16, 'vi', 4);
+console.log(vitoria);
---
+++ src/tiposGenericos/test.ts
@@ -0,0 +1,4 @@
+function aumentar(palavra: string): string {
+  return palavra.toUpperCase();
+}
+console.log(aumentar('walter peixoto'));
